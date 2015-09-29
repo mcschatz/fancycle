10fb29de56d84b4ad8e0fd56dc8847a2a9a8c495
Mimi Schatz <mcschatz@gmail.com>
2015-09-29 11:10:24 -0600

    added hidden toggle, counter, and coloring

--- fancycle.js
+++ fancycle.js
@@ -1,1 +1,33 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+  toggleSong('#justin', '#baby');
+  toggleSong('#miley', '#wrecking-ball');
+  toggleSong('#selena', '#come-and-get-it');
+
+  colorizeSong('#baby');
+  colorizeSong('#wrecking-ball');
+  colorizeSong('#come-and-get-it');
+
+
+  function toggleSong(artistID, songID) {
+    $(artistID).click(function () {
+      $(songID).toggleClass('hidden');
+      increaseViews(songID);
+    });
+  };
+
+  function increaseViews(songID) {
+    if ($(songID).attr("class") === "song") {
+      var counter = $(songID).find("#view-counter").text();
+      counter ++;
+      $(songID).find("#view-counter").text(counter);
+    };
+  };
+
+  function colorizeSong(songID) {
+    var song = $(songID);
+
+    song.find('#colorize').click(function(){
+      song.toggleClass("text-color")
+    });
+  };
+});
